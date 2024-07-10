2cf93ca550b9adcdd74afae81a857f05864d483c
walber <walberoc@gmail.com>
2024-07-10 05:33:58 -0300

    cadastro inicial de Pessoa

---
+++ mokito-dio/src/main/java/mokito/example/ApiDosCorreios.java
@@ -0,0 +1,11 @@
+package mokito.example;
+
+
+public class ApiDosCorreios {
+
+    public ApiDosCorreios() {
+    }
+    public DadosLocalizacao buscaDadosComBaseNoCep(String cep) {
+        return null;
+    }
+}
