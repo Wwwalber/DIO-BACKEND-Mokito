ca3dc81c9a84c57a6589c39d9cf2a470aad09286
walber <walberoc@gmail.com>
2024-07-10 05:37:02 -0300

    uma forma de usar Mokito para teste

---
+++ mokito-dio/src/test/java/CadastrarPessoaTeste.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import mokito.example.ApiDosCorreios;
+
+@ExtendWith(MockitoExtension.class)
+public class CadastrarPessoaTeste {
+
+    @Mock
+    private ApiDosCorreios apiDosCorreios;
+    
+}
